7b531cee87c2bb083d42b3959b53b8c811b4c365
Ivan Dai <ivandaixivwork@gmail.com>
2019-11-18 10:18:33 -0800

    Footer content implemented

--- themes/inhabitent/footer.php
+++ themes/inhabitent/footer.php
@@ -11,8 +11,32 @@
 		</div>
 			<footer id="colophon" class="site-footer" role="contentinfo">
 				<div class="site-info">
-					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
+					<div class="contact-info">
+						<h2>Contact Info</h2>
+						<ul>
+							<li><i class="fas fa-envelope"></i><a href="mailto:info@inhabitent.com">info@inhabitent.com</a></li>
+							<li><i class="fas fa-phone-alt"></i><a href="tel:778-456-7891">778-456-7891</a></li>
+						</ul>
+						<ul>
+							<li class="social-media"><i class="fab fa-facebook-square"></i></li>
+							<li class="social-media"><i class="fab fa-twitter-square"></i></li>
+							<li class="social-media"><i class="fab fa-google-plus-square"></i></li>
+						</ul>
+					</div>
+					<div class="biz-hours">
+						<h2>Business Hours</h2>
+						<ul>
+							<li>Monday-Friday: 9AM to 5PM</li>
+							<li>Saturday: 10am to 2pm</li>
+							<li>Sunday: Closed</li>
+						</ul>
+					</div>
+					<div class="footer-logo">
+						<img src="<?php echo get_template_directory_uri();?>/images/logos/inhabitent-logo-text-dark.svg" alt="Inhabitent Logo">
+					</div> 
+					<!-- <a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a> -->
 				</div><!-- .site-info -->
+				<p>COPYRIGHT &copy 2019 INHABITENT</p>
 			</footer><!-- #colophon -->
 		</div><!-- #page -->
 
