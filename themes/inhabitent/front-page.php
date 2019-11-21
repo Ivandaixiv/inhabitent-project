d3ebd9d30678be38acbe9a1741e9b4768fe99fa3
Ivan Dai <ivandaixivwork@gmail.com>
2019-11-21 15:08:38 -0800

    Loop for do/eat/wear/sleep added

--- themes/inhabitent/front-page.php
+++ themes/inhabitent/front-page.php
@@ -11,6 +11,26 @@ get_header(); ?>
 		<main id="main" class="site-main" role="main">
 			<div class="banner-image" style=" background-image: linear-gradient(rgba(0, 0, 0, .4),rgba(0, 0, 0, .4)), url(<?php if ( has_post_thumbnail() ) {echo get_the_post_thumbnail_url();} ?>); background-size: cover; " >
 			</div>
+			<section class="product-info container">
+            <h2>Shop Stuff</h2>
+            <?php
+               $terms = get_terms( array(
+                   'taxonomy' => 'product_type',
+                   'hide_empty' => 0,
+               ) );
+               if ( ! empty( $terms ) && ! is_wp_error( $terms ) ) :
+            ?>
+               <div class="product-type-blocks">
+                  <?php foreach ( $terms as $term ) : ?>
+                     <div class="product-type-block-wrapper">
+                        <img src="<?php echo get_template_directory_uri() . '/images/product-type-icons/' . $term->slug; ?>.svg" alt="<?php echo $term->name; ?>" />
+                        <p><?php echo $term->description; ?></p>
+                        <p><a href="<?php echo get_term_link( $term ); ?>" class="btn"><?php echo $term->name; ?> Stuff</a></p>
+                     </div>
+                  <?php endforeach; ?>
+               </div>
+            <?php endif; ?>
+         </section>
 			<img class="full-logo" src=" <?php echo get_template_directory_uri();?>/images/logos/inhabitent-logo-full.svg);">
 				<?php
 			$args = array(
