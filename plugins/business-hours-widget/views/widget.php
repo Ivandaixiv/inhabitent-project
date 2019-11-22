8cefd8c3dc964de3205a5fb5e998b83fe231b27d
Ivan Dai <ivandaixivwork@gmail.com>
2019-11-22 15:19:41 -0800

    Sidebar stylized

--- plugins/business-hours-widget/views/widget.php
+++ plugins/business-hours-widget/views/widget.php
@@ -1,31 +1,37 @@
 <!-- This file is used to markup the public-facing widget. -->
 <?php if( strlen( trim( $monday_friday)) > 0) :?>
-    <p>
-        <span class="day-of-week">
-            Monday - Friday
-        </span>
-    </p>
-    <p>
-        <?php echo $monday_friday?>
-    </p>
+    <div>
+        <p>
+            <span class="day-of-week">
+                Monday - Friday
+            </span>
+        </p>
+        <p>
+            <?php echo $monday_friday?>
+        </p>
+    </div>
 <?php endif;?>
 <?php if( strlen( trim( $saturday)) > 0) :?>
-    <p>
-        <span class="day-of-week">
-            Saturday
-        </span>
-    </p>
-    <p>
-        <?php echo $saturday?>
-    </p>
+    <div>
+        <p>
+            <span class="day-of-week">
+                Saturday
+            </span>
+        </p>
+        <p>
+            <?php echo $saturday?>
+        </p>
+    </div>
 <?php endif;?>
 <?php if( strlen( trim( $sunday)) > 0) :?>
-    <p>
-        <span class="day-of-week">
-            Sunday
-        </span>
-    </p>
-    <p>
-        <?php echo $sunday?>
-    </p>
+    <div>
+        <p>
+            <span class="day-of-week">
+                Sunday
+            </span>
+        </p>
+        <p>
+            <?php echo $sunday?>
+        </p>
+    </div>
 <?php endif;?>
--- themes/inhabitent/sidebar.php
+++ themes/inhabitent/sidebar.php
@@ -12,10 +12,12 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 
 <div id="secondary" class="widget-area" role="complementary">
 	<div class="widget-area-container">
-		<i class="fas fa-phone-alt"></i>
-		<i class="fas fa-envelope"></i>
-		<i class="fas fa-map-marker-alt"></i> 1490 W Broadway <br>
-		Vancouver, BC V6H 1H5
+		<aside class="widget-contact">
+			<h2>Contact Info</h2>
+			<i class="fas fa-phone-alt"><a href="tel:7784567891"> 778-456-7891</a></i>
+			<i class="fas fa-envelope"><a href="mailto:info@inhabitent.com"> info@inhabitent.com</a></i>
+			<i class="fas fa-map-marker-alt"> <span>1490 W Broadway<br>Vancouver, BC V6H 1H5</span></i>
+		</aside>
 		<?php dynamic_sidebar( 'sidebar-1' ); ?>
 	</div>
 </div><!-- #secondary -->
