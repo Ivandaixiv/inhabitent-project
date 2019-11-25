0ff89d61e72a9c4f27077b3e2a1d3f811bbe7906
Ivan Dai <ivandaixivwork@gmail.com>
2019-11-25 15:27:58 -0800

    Featured journals now uses grid and has border

--- themes/inhabitent/front-page.php
+++ themes/inhabitent/front-page.php
@@ -12,7 +12,7 @@ get_header(); ?>
 			<div class="banner-image" style=" background-image: linear-gradient(rgba(0, 0, 0, .4),rgba(0, 0, 0, .4)), url(<?php if ( has_post_thumbnail() ) {echo get_the_post_thumbnail_url();} ?>); background-size: cover; " >
 			</div>
 			<section class="product-info container">
-            <h2>Shop Stuff</h2>
+            <h2 class="frontpage-title">Shop Stuff</h2>
 				<?php
 				$terms = get_terms( array(
 					'taxonomy' => 'product_type',
@@ -31,27 +31,33 @@ get_header(); ?>
 				</div>
 				<?php endif; ?>
 			</section>
-			<section class="featured-journals">
+			<section class="featured-journals-wrapper width-restriction">
 				<img class="full-logo" src=" <?php echo get_template_directory_uri();?>/images/logos/inhabitent-logo-full.svg);">
+				<h2 class="frontpage-title">Inhabitent Journals</h2>	
+				<div class="featured-journals">
 					<?php
-				$args = array(
-					'order' => 'ASC',
-					'orderby' => 'rand',
-					'posts_per_page' => 3,
-					'post_type' => 'post',
-				);
-				$journal = new WP_Query( $args );
-				?>	   
-				<?php if ( $journal->have_posts() ) : ?>
-				<?php while ( $journal->have_posts() ) : $journal->the_post(); ?>
-					<?php the_post_thumbnail(array(375,250))?>
-					<h1><?php the_title(); ?></h1>
-					<?php endwhile; ?>
-					<?php the_posts_navigation(); ?>
-					<?php wp_reset_postdata(); ?>
-					<?php else : ?> 
-					<h2>Nothing found!</h2>
-				<?php endif; ?>
+					$args = array(
+						'order' => 'ASC',
+						'posts_per_page' => 3,
+						'post_type' => 'post',
+					);
+					$journal = new WP_Query( $args );
+					?>	   
+					<?php if ( $journal->have_posts() ) : ?>
+					<?php while ( $journal->have_posts() ) : $journal->the_post(); ?>
+						<div class="featured-journals-box">
+							<span class="featured-journals-image"><?php the_post_thumbnail('large')?></span>
+							<div class="featured-journals-info">
+								<h1><?php the_title(); ?></h1>
+							</div>
+						</div>
+						<?php endwhile; ?>
+						<?php the_posts_navigation(); ?>
+						<?php wp_reset_postdata(); ?>
+						<?php else : ?> 
+						<h2>Nothing found!</h2>
+					<?php endif; ?>
+				</div>
 			</section>
 		</main><!-- #main -->
 	</div><!-- #primary -->
