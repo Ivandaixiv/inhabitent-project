0f9b14cce995571d62c1ab019e6734f660da1210
Ivan Dai <ivandaixivwork@gmail.com>
2019-11-25 14:05:29 -0800

    Shop stuff section on homepage stylized

--- themes/inhabitent/front-page.php
+++ themes/inhabitent/front-page.php
@@ -13,43 +13,46 @@ get_header(); ?>
 			</div>
 			<section class="product-info container">
             <h2>Shop Stuff</h2>
-            <?php
-               $terms = get_terms( array(
-                   'taxonomy' => 'product_type',
-                   'hide_empty' => 0,
-               ) );
-               if ( ! empty( $terms ) && ! is_wp_error( $terms ) ) :
-            ?>
-               <div class="product-type-blocks">
-                  <?php foreach ( $terms as $term ) : ?>
-                     <div class="product-type-block-wrapper">
-                        <img src="<?php echo get_template_directory_uri() . '/images/product-type-icons/' . $term->slug; ?>.svg" alt="<?php echo $term->name; ?>" />
-                        <p><?php echo $term->description; ?></p>
-                        <p><a href="<?php echo get_term_link( $term ); ?>" class="btn"><?php echo $term->name; ?> Stuff</a></p>
-                     </div>
-                  <?php endforeach; ?>
-               </div>
-            <?php endif; ?>
-         </section>
-			<img class="full-logo" src=" <?php echo get_template_directory_uri();?>/images/logos/inhabitent-logo-full.svg);">
 				<?php
-			$args = array(
-				'order' => 'ASC',
-				'posts_per_page' => 3,
-				'post_type' => 'post',
-			);
-			$journal = new WP_Query( $args );
-		?>	   
-			<?php if ( $journal->have_posts() ) : ?>
-			<?php while ( $journal->have_posts() ) : $journal->the_post(); ?>
-				<h1><?php the_title(); ?></h1>
-				<?php the_post_thumbnail(array(375,250))?>
-				<?php endwhile; ?>
-				<?php the_posts_navigation(); ?>
-				<?php wp_reset_postdata(); ?>
-				<?php else : ?> 
-				<h2>Nothing found!</h2>
-			<?php endif; ?>
+				$terms = get_terms( array(
+					'taxonomy' => 'product_type',
+					'hide_empty' => 0,
+				) );
+				if ( ! empty( $terms ) && ! is_wp_error( $terms ) ) :
+				?>
+				<div class="product-type-blocks">
+					<?php foreach ( $terms as $term ) : ?>
+						<div class="product-type-block-wrapper">
+							<img src="<?php echo get_template_directory_uri() . '/images/product-type-icons/' . $term->slug; ?>.svg" alt="<?php echo $term->name; ?>" />
+							<p class="shop-icons"><?php echo $term->description; ?></p>
+							<p class="theme-button-green"><a href="<?php echo get_term_link( $term ); ?>" class="btn"><?php echo $term->name; ?> Stuff</a></p>
+						</div>
+					<?php endforeach; ?>
+				</div>
+				<?php endif; ?>
+			</section>
+			<section class="featured-journals">
+				<img class="full-logo" src=" <?php echo get_template_directory_uri();?>/images/logos/inhabitent-logo-full.svg);">
+					<?php
+				$args = array(
+					'order' => 'ASC',
+					'orderby' => 'rand',
+					'posts_per_page' => 3,
+					'post_type' => 'post',
+				);
+				$journal = new WP_Query( $args );
+				?>	   
+				<?php if ( $journal->have_posts() ) : ?>
+				<?php while ( $journal->have_posts() ) : $journal->the_post(); ?>
+					<?php the_post_thumbnail(array(375,250))?>
+					<h1><?php the_title(); ?></h1>
+					<?php endwhile; ?>
+					<?php the_posts_navigation(); ?>
+					<?php wp_reset_postdata(); ?>
+					<?php else : ?> 
+					<h2>Nothing found!</h2>
+				<?php endif; ?>
+			</section>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
