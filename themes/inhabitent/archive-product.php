4c7a5e9dc06da18a7940a738cd87e6ef66a27010
Ivan Dai <ivandaixivwork@gmail.com>
2019-11-26 15:19:33 -0800

    Photos Added

--- themes/inhabitent/archive-product.php
+++ themes/inhabitent/archive-product.php
@@ -9,24 +9,36 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 			<h2 class="frontpage-title">Shop Stuff</h2>
-		<?php if ( have_posts() ) : ?>
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
-
+			<section class="shop-container">
 				<?php
-					get_template_part( 'template-parts/content' );
-				?>
-
-			<?php endwhile; ?>
-
-			<?php the_posts_navigation(); ?>
-
-		<?php else : ?>
-
-			<?php get_template_part( 'template-parts/content', 'none' ); ?>
-
-		<?php endif; ?>
-
+					$args = array(
+						'order' => 'ASC',
+						'posts_per_page' => 16,
+						'post_type' => 'product',
+					);
+					$shop = new WP_Query( $args );
+				?>	   
+				<?php if ( $shop->have_posts() ) : ?>
+				<?php while ( $shop->have_posts() ) : $shop->the_post(); ?>
+					<div class="featured-journals-box">
+						<span class="featured-journals-image">
+							<?php the_post_thumbnail()?>
+						</span>
+						<div class="featured-journals-info">
+							<h1><?php the_title(); ?></h1>
+						</div>
+					</div>
+					<?php   $post = get_post(); 
+							$price = $post->price;
+							echo $price;
+					?>
+					<?php endwhile; ?>
+					<?php the_posts_navigation(); ?>
+					<?php wp_reset_postdata(); ?>
+					<?php else : ?> 
+					<h2>Nothing found!</h2>
+				<?php endif; ?>
+			</section>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
